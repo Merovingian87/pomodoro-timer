58e64e13bba78feb2afd39c0a24b88c571222e3b
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2019-08-02 19:38:01 -0500

    add files

---
+++ clock.js
@@ -0,0 +1,34 @@
+"use strict"
+
+
+    function showTime() {
+      let date = new Date();
+      let h = date.getHours();
+      let m = date.getMinutes();
+      let s = date.getSeconds();
+      let session = "AM";
+
+          if (h == 0) {
+            h = 12;
+          }
+          if (h > 12) {
+            h -= 12;
+            session = "PM";
+          }
+
+          h = (h < 10) ? "0" + h : h;
+          m = (m < 10) ? "0" + m : m;
+          s = (s < 10) ? "0" + s : s;
+
+
+      let time = h + " : " + m + " : " + s + " " + session;
+
+      document.getElementById("MyClockDisplay").innerText = time;
+      document.getElementById("MyClockDisplay").textContent = time;
+
+      setTimeout(showTime,1000);
+    }
+
+
+  showTime();
+  // setInterval(showTime, 1000);
